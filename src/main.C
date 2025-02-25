4b150e17ae378760ee0a96667f785ea8ea9452e4
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-02-25 16:02:25 +0800

    Add texture Oiiaii complete

--- src/main.C
+++ src/main.C
@@ -1,19 +1,42 @@
+/*
+  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely.
+*/
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-
+#include <SDL3_image/SDL_image.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_Texture *texture = NULL;
+
+extern unsigned char icon_bmp[];
+extern unsigned int icon_bmp_len;
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     /* Create the window */
     if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
-        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        SDL_Log("Couldn't create window and renderer: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    /* Load the icon */
+    texture = IMG_LoadTexture(renderer, "Assets/Images/image.png");
+    if (!texture) {
+        SDL_Log("Couldn't load icon: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
     return SDL_APP_CONTINUE;
 }
 
@@ -30,22 +53,21 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const char *message = "Hello World!";
     int w = 0, h = 0;
-    float x, y;
-    const float scale = 4.0f;
+    SDL_FRect dst;
+    const float scale = 1.0f;
 
-    /* Center the message and scale it up */
+    /* Center the icon and scale it up */
     SDL_GetRenderOutputSize(renderer, &w, &h);
     SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+    SDL_GetTextureSize(texture, &dst.w, &dst.h);
+    dst.x = ((w / scale) - dst.w) / 2;
+    dst.y = ((h / scale) - dst.h) / 2;
 
-    /* Draw the message */
+    /* Draw the icon */
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
+    SDL_RenderTexture(renderer, texture, NULL, &dst);
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
