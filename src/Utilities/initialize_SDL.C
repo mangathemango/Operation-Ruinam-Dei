4715d859e87ffaeb0e5919a155427622ade62952
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-02-28 10:30:05 +0800

    Add some comments inside Initialize_SDL

--- src/Utilities/initialize_SDL.C
+++ src/Utilities/initialize_SDL.C
@@ -4,22 +4,23 @@
 *   Initializes everything related to SDL: window, renderer, image loading, and so on.
 */
 int Initialize_SDL() {
+    // Initialize SDL and SDL_image
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
 
     // Set up window
-    app.setup.window = SDL_CreateWindow(app.config.window_title,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        app.config.window_width, app.config.window_height, 
-        app.config.window_fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
+    app.setup.window = SDL_CreateWindow(
+        app.config.window_title,                                                // Set window title
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,                         // Centers the window
+        app.config.window_width, app.config.window_height,                      // Set window size
+        app.config.window_fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);      // Set fullscreen
     
     if (!app.setup.window) {
         SDL_Log("Failed to create window: %s", SDL_GetError());
         return 1;
     }
 
-    SDL_SetWindowPosition(app.setup.window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-    SDL_RaiseWindow(app.setup.window);
+    SDL_RaiseWindow(app.setup.window);                                          
 
 
     // Set up renderer
