3e274c647992b7dd2c8ae64e2d519e4dab14437c
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-03-01 18:31:34 +0800

    Remove old app.time

--- src/App/app_preupdate.C
+++ src/App/app_preupdate.C
@@ -1,17 +1,11 @@
 #include <app.h>
 #include <input.h>
+#include <time_manager.h>
 
 /* 
 *   This function is called every frame of the program BEFORE App_Event_Handler().
 */
 int App_PreUpdate() {
-    // Update delta time
-    static Uint64 previousTicks = 0;
-    Uint64 currentTicks = SDL_GetPerformanceCounter();
-    app.time.deltaTime = (float)(currentTicks - previousTicks) / SDL_GetPerformanceFrequency();
-    previousTicks = currentTicks;
-
-    
     Input_PreUpdate();
     return 0;
 }
