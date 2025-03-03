e61f3b7808aab847d7d664e4e7db8e0b007823dd
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-03-04 01:07:01 +0800

    Add semi working particle system

---
+++ include/Utilities/particles.h
@@ -0,0 +1,29 @@
+// #include <SDL.h>
+// #include <vec2.h>
+// #include <stdbool.h>
+
+// typedef struct {
+//     Vec2 position;
+//     Vec2 velocity;
+//     float lifetime;
+//     void (*custom_Movement)(Particle*);
+// } Particle;
+
+
+// typedef struct {
+//     Vec2 position;
+//     Vec2 direction;
+//     float angleRange;           
+//     float emissionRate;         // Time between emissions in seconds
+//     int emissionNumber;         // Number of particles to emit per emission
+//     int maxParticles;
+//     float particleLifetime;
+//     float particleVelocity;
+//     Particle* particles;
+// } ParticleEmitter;
+
+// ParticleEmitter* Particle_CreateEmittor(Vec2 position, Vec2 direction, float angleRange,
+//                                          float emissionRate, int maxParticles, float particleLifetime,
+//                                          float particleVelocity);
+
+// ParticleEmitter* Particle_CreateEmptyEmitter();
